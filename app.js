198b50000aed29ee3dc5bc79ba40f3ae664c2208
gitztech <gjpyzer+github@gmail.com>
2016-04-09 21:32:39 +0100

    Improvements to architecture

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 (function() {
-  var DEBUG = true;
 
+  var DEBUG = true;
   var questions = [];
   var currentQuestion = 0;
 
-  initQuestions();
+  function initApp() {
+    initQuestions();
 
-  if (questions.length) {
-    renderQuestion(0, prepareDOM);
+    if (questions.length) {
+      new QuestionView($('#questions'), questions[0]);
 
-    $("#nextQuestion").on("click", function() {
-      renderQuestion(++currentQuestion, prepareDOM);
-    });
-  }
-
-  // set up events
-  function prepareDOM(question) {
-    var textField = $('#textField').get(0);
-    textField.selectionStart = question.defaultCursorPosition;
-    textField.focus();
-
-    // bind the main key events where a cursor change may occur
-    $("#textField").bind("keyup click focus", function() {
-      checkCursorPosition(this, question);
-    });
+      $("#nextQuestion").on("click", function() {
+        new QuestionView($('#questions'), questions[++currentQuestion]);
+      });
+    }
   }
 
   function initQuestions() {
     questions.push(new Question(
       {
+        index: 0,
         title: "Using LEFT and RIGHT keys only, move the cursor to the position before the \"s\" in \"some\"",
         sampleText: "This is some sample text",
         defaultCursorPosition: 10,
@@ -37,6 +28,7 @@
 
      questions.push(new Question(
        {
+         index: 1,
          title: "Using Cmd + Left, move the cursor to the start of the line",
          sampleText: "This is some more sample text",
          defaultCursorPosition: 14,
@@ -45,41 +37,64 @@
 
   }
 
+  function QuestionView(element, question) {
+    this.element = $(element);
+    this.question = question;
+    this.render();
+  }
+
+  QuestionView.prototype = {
+      render: function() {
+        var template = $('#template').html();
+        Mustache.parse(template);
+
+        var rendered = Mustache.render(template, {
+          questionNo: (this.question.index + 1),
+          question: this.question.title,
+          sampleText: this.question.sampleText
+        });
+
+        this.element.html(rendered);
+        this.afterRender();
+      },
+
+      afterRender: function() {
+        this.ui = {};
+        this.ui.textField = this.element.find(".text-field");
+        this.ui.correct = this.element.find(".correct");
+
+        this.ui.textField.get(0).selectionStart = this.question.defaultCursorPosition;
+        this.ui.textField.focus();
+
+        var that = this;
+
+        // bind the main key events where a cursor change may occur
+        this.ui.textField.bind("keyup click focus", function() {
+          that.checkCursorPosition();
+        });
+      },
+
+      checkCursorPosition: function() {
+        var selectionStart = this.ui.textField.get(0).selectionStart;
+        var selectionEnd = this.ui.textField.get(0).selectionEnd;
+
+        if (DEBUG) {
+          console.log(selectionStart);
+          console.log(selectionEnd);
+        }
+
+        var show = (selectionStart == this.question.correctPositionCursorPosition && selectionEnd == this.question.correctPositionCursorPosition);
+        this.ui.correct.toggleClass("hidden", !show);
+      }
+  }
+
   function Question(q) {
+    this.index = q.index;
     this.title = q.title;
     this.sampleText = q.sampleText;
     this.defaultCursorPosition = q.defaultCursorPosition;
     this.correctPositionCursorPosition = q.correctCursorPosition;
   }
 
-  function checkCursorPosition(textField, question) {
-    var selectionStart = textField.selectionStart;
-    var selectionEnd = textField.selectionEnd;
-
-    var correct = $('#correct');
-
-    if (DEBUG) {
-      console.log(selectionStart);
-      console.log(selectionEnd);
-    }
-
-    var show = (selectionStart == question.correctPositionCursorPosition && selectionEnd == question.correctPositionCursorPosition);
-    correct.toggleClass("hidden", !show);
-  }
-
-  function renderQuestion(id, callback) {
-    var question = questions[id];
-
-    var template = $('#template').html();
-    Mustache.parse(template);
-
-    var rendered = Mustache.render(template, {
-      questionNo: (id+1),
-      question: question.title,
-      sampleText: question.sampleText
-    });
-
-    $('#questions').html(rendered);
-    callback(question);
-  }
+  initApp();
 })();
