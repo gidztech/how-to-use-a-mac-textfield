bbd3a1df99448cadde5ad21c1b19cb9e72a334c8
gitztech <gjpyzer+github@gmail.com>
2016-04-09 22:37:56 +0100

    More improvements to structure

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 (function() {
-
-  var DEBUG = true;
   var questions = [];
   var currentQuestion = 0;
 
@@ -37,56 +35,59 @@
 
   }
 
-  function QuestionView(element, question) {
+  function BaseView(element, templateData) {
     this.element = $(element);
+    this.render(templateData); // Calls the render() of the QuestionView instance (this)
+  }
+
+  BaseView.prototype = {
+    render: function(templateData) {
+      var templateTag = $("#" + this.template).html();
+      Mustache.parse(templateTag);
+      var rendered = Mustache.render(templateTag, templateData);
+      this.element.html(rendered);
+      this.afterRender();
+    },
+  }
+
+  function QuestionView(element, question) {
     this.question = question;
-    this.render();
+    // Call super constructor using the QuestionView context
+    BaseView.call(this, element, {
+      questionNo: (this.question.index + 1),
+      question: this.question.title,
+      sampleText: this.question.sampleText
+    });
   }
 
-  QuestionView.prototype = {
-      render: function() {
-        var template = $('#template').html();
-        Mustache.parse(template);
+  // QuestionView extends BaseView
+  QuestionView.prototype = Object.create(BaseView.prototype);
 
-        var rendered = Mustache.render(template, {
-          questionNo: (this.question.index + 1),
-          question: this.question.title,
-          sampleText: this.question.sampleText
-        });
+  QuestionView.prototype.afterRender = function() {
+    this.ui = {};
+    this.ui.textField = this.element.find(".text-field");
+    this.ui.correct = this.element.find(".correct");
 
-        this.element.html(rendered);
-        this.afterRender();
-      },
+    this.ui.textField.get(0).selectionStart = this.question.defaultCursorPosition;
+    this.ui.textField.focus();
 
-      afterRender: function() {
-        this.ui = {};
-        this.ui.textField = this.element.find(".text-field");
-        this.ui.correct = this.element.find(".correct");
+    var that = this;
 
-        this.ui.textField.get(0).selectionStart = this.question.defaultCursorPosition;
-        this.ui.textField.focus();
+    // bind the main key events where a cursor change may occur
+    this.ui.textField.bind("keyup click focus", function() {
+      that.checkCursorPosition();
+    });
+  };
 
-        var that = this;
+  QuestionView.prototype.checkCursorPosition = function() {
+    var selectionStart = this.ui.textField.get(0).selectionStart;
+    var selectionEnd = this.ui.textField.get(0).selectionEnd;
 
-        // bind the main key events where a cursor change may occur
-        this.ui.textField.bind("keyup click focus", function() {
-          that.checkCursorPosition();
-        });
-      },
+    var show = (selectionStart == this.question.correctPositionCursorPosition && selectionEnd == this.question.correctPositionCursorPosition);
+    this.ui.correct.toggleClass("hidden", !show);
+  };
 
-      checkCursorPosition: function() {
-        var selectionStart = this.ui.textField.get(0).selectionStart;
-        var selectionEnd = this.ui.textField.get(0).selectionEnd;
-
-        if (DEBUG) {
-          console.log(selectionStart);
-          console.log(selectionEnd);
-        }
-
-        var show = (selectionStart == this.question.correctPositionCursorPosition && selectionEnd == this.question.correctPositionCursorPosition);
-        this.ui.correct.toggleClass("hidden", !show);
-      }
-  }
+  QuestionView.prototype.template = "questionTemplate";
 
   function Question(q) {
     this.index = q.index;
