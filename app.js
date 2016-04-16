e45899a288f9ef5bc1680923202529c82632a9c0
gitztech <gjpyzer+github@gmail.com>
2016-04-16 20:56:19 +0100

    Create multiple types of questions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function() {
   var questions = [];
-  var currentQuestion = 0;
+  var currentQuestionIndex = 0;
+  var nextQuestion = $(".nextQuestion");
 
   function initApp() {
     initQuestions();
@@ -8,24 +9,45 @@
     if (questions.length) {
       new QuestionView($('#questions'), questions[0]);
 
-      $("#nextQuestion").on("click", function() {
-        if (questions[currentQuestion + 1] !== undefined) {
-          new QuestionView($('#questions'), questions[++currentQuestion]);
-        }
+      nextQuestion.on("click", function() {
+        new QuestionView($('#questions'), questions[++currentQuestionIndex]);
+        nextQuestion.toggleClass("hidden", true);
       });
     }
   }
 
-  function Question(q) {
+  function BaseQuestion(q) {
     this.index = q.index;
     this.title = q.title;
     this.value = q.value;
+
     this.defaultCursorPosition = q.defaultCursorPosition;
-    this.correctCursorPosition = q.correctCursorPosition;
   }
 
+  function CursorQuestion(q) {
+    this.correctCursorPosition = q.correctCursorPosition;
+
+    // Call the super constructor (BaseQuestion) using the CursorQuestion context as "this"
+    BaseQuestion.call(this, q);
+  }
+
+  function SelectionQuestion(q) {
+    this.selectedRange = q.selectedRange;
+
+    // Call the super constructor (BaseQuestion) using the SelectionQuestion context as "this"
+    BaseQuestion.call(this, q);
+  }
+
+  // CursorQuestion extends BaseQuestion
+  CursorQuestion.prototype = Object.create(BaseQuestion.prototype);
+  CursorQuestion.prototype.constructor = CursorQuestion;
+  // SelectionQuestion extends BaseQuestion
+  SelectionQuestion.prototype = Object.create(BaseQuestion.prototype);
+  SelectionQuestion.prototype.constructor = SelectionQuestion;
+
+
   function initQuestions() {
-    questions.push(new Question(
+    questions.push(new CursorQuestion(
       {
         index: 0,
         title: "Using LEFT and RIGHT keys only, move the cursor to the position before the \"s\" in \"some\"",
@@ -34,7 +56,7 @@
         correctCursorPosition: 8
      }));
 
-     questions.push(new Question(
+     questions.push(new CursorQuestion(
        {
          index: 1,
          title: "Using Cmd + Left/Right, move the cursor to the start of the line",
@@ -43,13 +65,13 @@
          correctCursorPosition: 0
       }));
 
-      questions.push(new Question(
+      questions.push(new SelectionQuestion(
         {
           index: 2,
           title: "Using Cmd + Shift + Right, select all text to the right of the space before \"sample\"",
           value: "This is some more sample text",
           defaultCursorPosition: 18,
-          correctCursorPosition: 0
+          selectedRange: { start: 18, end: 29}
        }));
 
   }
@@ -93,6 +115,7 @@
     this.ui = {};
     this.ui.textField = this.element.find(".text-field");
     this.ui.correct = this.element.find(".correct");
+    this.ui.nextQuestion = this.element.find(".nextQuestion");
 
     this.ui.textField.get(0).selectionStart = this.question.defaultCursorPosition;
     this.ui.textField.focus();
@@ -101,16 +124,37 @@
 
     // bind the main key events where a cursor change may occur
     this.ui.textField.bind("keyup click focus", function() {
-      that.checkCursorPosition();
+      var currentQuestion = questions[currentQuestionIndex];
+
+      if (currentQuestion.constructor == CursorQuestion) {
+        that.checkCursorPosition();
+      }
+      if (currentQuestion.constructor == SelectionQuestion) {
+        that.checkCurrentSelection();
+      }
     });
   };
+
+  QuestionView.prototype.checkCurrentSelection = function() {
+    console.log("Test");
+  }
 
   QuestionView.prototype.checkCursorPosition = function() {
     var selectionStart = this.ui.textField.get(0).selectionStart;
     var selectionEnd = this.ui.textField.get(0).selectionEnd;
 
     var show = (selectionStart == this.question.correctCursorPosition && selectionEnd == this.question.correctCursorPosition);
+    var moreQuestionsExist = (questions[currentQuestionIndex + 1] !== undefined);
     this.ui.correct.toggleClass("hidden", !show);
+    // show = true, moreQuestionsExist = true
+      // false || false => false => visible
+    // show = true, moreQuestionsExist = false
+      // false || true => true => hidden
+    // show = false, moreQuestionsExist = true
+      // true || false => true => hidden
+    // show = false, moreQuestionsExist = false
+      // true || true => hidden
+    nextQuestion.toggleClass("hidden", (!show || !moreQuestionsExist));
   };
 
   initApp();
